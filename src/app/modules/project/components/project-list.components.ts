c96f9c20f6e0269148f0ec105ff165262ce80ea5
Nagaitsev Sergey <tech@eventarium.pro>
2018-06-16 12:24:36 +0300

    Remove images

--- src/app/modules/project/components/project-list.components.ts
+++ src/app/modules/project/components/project-list.components.ts
@@ -3,38 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-project-list',
     template: `
-    <div class="columns is-multiline is-mobile">
-      <div class="column is-one-third">
+    <div class="columns is-multiline">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
         </div>
       </div>
-      <div class="column is-one-third">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
         </div>
       </div>
-      <div class="column is-one-third">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
         </div>
       </div>
-      <div class="column is-one-third">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
         </div>
       </div>
-      <div class="column is-one-third">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
         </div>
       </div>
-      <div class="column is-one-third">
+      <div class="column is-one-third is-half-tablet">
         <div class="box">
           <p class="title">Title</p>
           <p class="subtitle">Subtitle</p>
